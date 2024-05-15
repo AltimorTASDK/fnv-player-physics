940e69b9c81db88731322fe89cb6726da2a66fcc
AltimorTASDK <altimormc@gmail.com>
2024-05-15 10:49:54 -0400

    add acceleration

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <cstddef>
 #include <Windows.h>
 
+using enum hkpCharacterState::StateType;
+
 struct IniPrefSetting {
 	void **vtable;
 	float value;
@@ -25,25 +27,69 @@ struct CharacterMoveParams {
 };
 
 namespace ini {
-auto fAcceleration = IniPrefSetting("fAcceleration:Movement", 5.f);
-auto fFriction = IniPrefSetting("fAcceleration:Friction", 5.f);
-auto fStopSpeed = IniPrefSetting("fAcceleration:StopSpeed", .3f);
+//auto fAcceleration = IniPrefSetting("fAcceleration:Movement", 5.f);
+//auto fFriction = IniPrefSetting("fFriction:Movement", 5.f);
+//auto fStopSpeed = IniPrefSetting("fStopSpeed:Movement", .3f);
+constexpr auto fFriction = 5.f;
+constexpr auto fAcceleration = 6.f;
+constexpr auto fAirAcceleration = 10.f;
+constexpr auto fStopSpeed = 8.f;
+constexpr auto fAirSpeed = .1f;
 }
 
 static void ApplyFriction(
-	bhkCharacterController *charCtrl,
 	const CharacterMoveParams &move,
 	AlignedVector4 *velocity,
 	float deltaTime)
 {
-	const auto speed = NiVector3(*velocity).Length();
-	const auto stopSpeed = move.maxSpeed * ini::fStopSpeed.value;
-	const auto friction = ini::fFriction.value * std::max(speed, stopSpeed) * dt;
+	const auto speed = ((NiVector3&)*velocity).Length();
+	const auto friction = ini::fFriction * std::max(speed, ini::fStopSpeed) * deltaTime;
 
 	if (friction >= speed)
 		*velocity = AlignedVector4(0, 0, 0, 0);
 	else
 		*velocity *= 1.f - friction / speed;
+}
+
+static void ApplyAcceleration(
+	const CharacterMoveParams &move,
+	AlignedVector4 *velocity,
+	UInt32 state,
+	const NiVector3 &moveVector,
+	float moveLength,
+	float deltaTime)
+{
+	const auto inAir = state == kState_InAir;
+	const auto speed = ((NiVector3&)*velocity).DotProduct(moveVector);
+	const auto maxSpeed = inAir ? moveLength * ini::fAirSpeed : moveLength;
+
+	if (speed < maxSpeed) {
+		const auto accelMultiplier = inAir ? ini::fAirAcceleration : ini::fAcceleration;
+		const auto accel = accelMultiplier * moveLength * deltaTime;
+		*velocity += moveVector * std::min(accel, maxSpeed - speed);
+	}
+}
+
+static void UpdateVelocity(
+	const CharacterMoveParams &move,
+	AlignedVector4 *velocity,
+	UInt32 state,
+	float deltaTime)
+{
+	if (state != kState_InAir)
+		ApplyFriction(move, velocity, deltaTime);
+
+	const auto moveLength = ((NiVector3&)move.input).Length();
+
+	if (moveLength < 1e-4f)
+		return;
+
+	const auto input = AlignedVector4(move.input * (1.f / moveLength));
+	const auto &forward = move.forward;
+	const auto &up = move.up;
+	const auto right = AlignedVector4(((NiVector3&)forward).CrossProduct(up));
+	const auto moveVector = forward * -input.x + right * input.y + up * input.z;
+	ApplyAcceleration(move, velocity, state, moveVector, moveLength, deltaTime);
 }
 
 static void hook_MoveCharacter(
@@ -57,18 +103,10 @@ static void hook_MoveCharacter(
 		return;
 	}
 
-	*velocity -= move.surfaceVelocity.PS();
-
+	const auto state = charCtrl->chrContext.hkState;
 	const auto deltaTime = charCtrl->stepInfo.deltaTime;
-
-	ApplyFriction(charCtrl, move, velocity, deltaTime);
-
-	const auto &forward = move.forward;
-	const auto &up = move.up;
-	const auto right = NiVector3(NiVector3(forward).CrossProduct(up));
-	const auto input = forward * -move.input.x + right * move.input.y + up * move.input.z;
-	*velocity += AlignedVector4(input) * .1f;
-
+	*velocity -= move.surfaceVelocity.PS();
+	UpdateVelocity(move, velocity, state, deltaTime);
 	*velocity += move.surfaceVelocity.PS();
 }
 
